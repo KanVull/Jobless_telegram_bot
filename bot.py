f2875741226658f3ac125ee1f214188c3429b7df
KanVull <minirorus7@gmail.com>
2022-06-09 15:31:19 +0300

    logging for +1 game, scoring

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import logger
 TELEBOT_ACCESS_TOKEN = os.environ.get('token')
 
 bot = telebot.TeleBot(TELEBOT_ACCESS_TOKEN, parse_mode=False)
+chat_id = None
 config = None
 games = None
 files = {
@@ -15,10 +16,11 @@ files = {
     'photo_sticker_answers': './stickers.txt'
 }
 
-
+def mem_of_a_day(time)
 
 @bot.message_handler(content_types=['photo'])
 def photo_message(photo):
+    chat_id = photo.chat.id
     number = random.randint(0,101)
     user_name = photo.from_user.first_name
     logger.log_info(f'photo gain {number} for {user_name}')
@@ -26,7 +28,7 @@ def photo_message(photo):
         with open(files['photo_text_answers'], 'r') as file:
             logger.log_info(f'reply to photo for {user_name} in text mode')
             lines = file.readlines()
-            bot.send_message(chat_id=photo.chat.id, text=f'{user_name}, {random.choice(lines)}')
+            bot.send_message(chat_id=chat_id, text=f'{user_name}, {random.choice(lines)}')
     elif (number+10) % 20 == 0:
         with open(files['photo_sticker_answers'], 'r') as file:
             logger.log_info(f'reply to photo for {user_name} in sticker mode')
@@ -35,13 +37,15 @@ def photo_message(photo):
 
 @bot.message_handler(regexp=r'^[0-9]')
 def echo(message):
+    chat_id = message.chat.id
     user_name = message.from_user.first_name
     logger.log_info(f'reply for number for {user_name}')
-    bot.send_message(chat_id=message.chat.id, text=f'{user_name}, ладно')
+    bot.send_message(chat_id=chat_id, text=f'{user_name}, ладно')
 
 def game_plus1(chat_id):
     score = int(config_data['games']['GAMES']['plus1']) + 1
     config_data['games']['GAMES']['plus1'] = str(score)
+    logger.log_info(f'\t\tscore: {score}')
     config_worker.save_config(config_data['games'], 'games')  
     
     if score % 1000 == 0:
@@ -53,6 +57,7 @@ def game_plus1(chat_id):
 
 @bot.message_handler(regexp=r'^(\+1)$')
 def gamePlus1(message):
+    chat_id = message.chat.id
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     logger.log_info(f'game +1 making for {user_name}')
@@ -69,12 +74,12 @@ def gamePlus1(message):
                 game_plus1(message.chat.id)
             else:
                 mess = 'чел, (T_T) Жди других'
-            bot.send_message(chat_id=message.chat.id, text=f'{user_name}, {mess}')
+            bot.send_message(chat_id=chat_id, text=f'{user_name}, {mess}')
             return
 
     counting = [user_id, 1]
-    game_plus1(message.chat.id)
-    bot.send_message(chat_id=message.chat.id, text=f'{user_name}, засчитано')
+    game_plus1(chat_id)
+    bot.send_message(chat_id=chat_id, text=f'{user_name}, засчитано')
 
 
 counting = None
