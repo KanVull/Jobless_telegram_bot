a159adbef23b28fc6c0790c1ea22b47e13bf858c
KanVull <minirorus7@gmail.com>
2022-06-09 14:51:11 +0300

    Change timezone for logger file on UTC+3

--- logger.py
+++ logger.py
@@ -1,7 +1,9 @@
 import datetime
 def current_data():
     now = datetime.datetime.now()
-    return now.strftime('%y:%m:%d %H:%M:%S')
+    time_change = datetime.timedelta(hours=3)
+    new_time = now + time_change
+    return new_time.strftime('%y:%m:%d %H:%M:%S')
 
 def log_info(message: str) -> None:
     print(f'[*] {current_data()} | {message}')
