bd148c358fc5721b5396044ebe09e5ebb48737a1
KanVull <minirorus7@gmail.com>
2022-07-12 13:55:47 +0300

    logger data chandeg desing

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ def current_data():
     now = datetime.datetime.now()
     time_change = datetime.timedelta(hours=3)
     new_time = now + time_change
-    return new_time.strftime('%y:%m:%d %H:%M:%S')
+    return new_time.strftime('%d.%m.%y %H:%M:%S')
 
 def log_info(message: str) -> None:
     print(f'[*] {current_data()} | {message}')
