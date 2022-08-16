3f8481c7d78faba48db825bd01da9c1ff36de7be
KanVull <minirorus7@gmail.com>
2022-08-16 16:11:01 +0300

    Little message fix

--- bot.py
+++ bot.py
@@ -106,7 +106,7 @@ def _add_balance(user_id: str, chat_id: str, user_name: str, amount: float) -> N
     r_balance = e.readble_amount(balance)
     bot.send_message(
         chat_id=chat_id, 
-        text=f'{user_name}, + {r_a} {_readble_amount_name(amount)}!\nВ кошельке: {r_balance}',
+        text=f'{user_name}, +{r_a} {_readble_amount_name(amount)}!\nВ кошельке: {r_balance}',
         disable_notification=True
     )
     logger.log_extrainfo(f"Added {e.readble_amount(r_a)} to balance: {e.readble_amount(balance - amount)} -> {r_balance}")
